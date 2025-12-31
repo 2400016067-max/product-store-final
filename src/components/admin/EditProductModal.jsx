63efaaa2be0cd566d7da9e7768e88312afb5dbd7
Imam Faqih Masduqi <2400016067.webmail.uad.ac.id>
2025-12-31 15:16:57 +0700

    perbaikan bug

--- src/components/admin/EditProductModal.jsx
+++ src/components/admin/EditProductModal.jsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Pencil } from "lucide-react"; 
 
-// 1. IMPORT DENGAN PATH YANG BENAR
+// 1. FIX: IMPORT DENGAN PATH YANG BENAR
 // Keluar dari admin (../), keluar dari components (../), masuk ke lib (/lib)
 import { CATEGORIES } from "../../lib/constants"; 
 
